54c98c3042f578453b11d21a67ba3ea5cc25dcbd
Rainier Paolo Punzalan <rpgpunzalan@rpinnotech.com>
2022-04-11 05:40:30 +0800

    add register endpoint

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,6 +3,7 @@ const { addUser } = require("../services/user.service");
 
 module.exports = {
     addUserHandler: async (req, res) => {
+        console.log(req.body)
         const user = await addUser(req.body);
         res.send(user)
     },
